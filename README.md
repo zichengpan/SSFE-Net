3eca49c69d2882696f83a17565d92ddc21aae8f8
zichengpan <42835617+zichengpan@users.noreply.github.com>
2024-07-02 15:30:57 +1000

    update

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ You can run the code by the following commands or simply use ```sh train.sh```.
     ```
 - Train Mini-ImageNet
     ```
-    python train.py -project ssfe -dataset mini_imagenet -base_mode 'ft_cos' -new_mode 'avg_cos' -gamma 0.1 -lr_base 0.1 -lr_new 0.1 -decay 0.0005 -epochs_base 100 -schedule Cosine -gpu 0 -temperature 16 -dataroot ./data -from_scratch
+    python train.py -project ssfe -dataset mini_imagenet -base_mode 'ft_cos' -new_mode 'avg_cos' -gamma 0.1 -lr_base 0.1 -lr_new 0.1 -decay 0.0005 -epochs_base 150 -schedule Cosine -gpu 0 -temperature 16 -dataroot ./data -from_scratch
     ```
 - Train PlantVillage
     ```
--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ python train.py -project ssfe -dataset cub200 -base_mode 'ft_cos' -new_mode 'avg
 -temperature 16 -dataroot ./data -batch_size_base 32 -from_scratch
 
 python train.py -project ssfe -dataset mini_imagenet -base_mode 'ft_cos' -new_mode 'avg_cos' -gamma 0.1 -lr_base 0.1 \
--lr_new 0.1 -decay 0.0005 -epochs_base 100 -schedule Cosine -gpu 0 -temperature 16 -dataroot ./data -from_scratch
+-lr_new 0.1 -decay 0.0005 -epochs_base 150 -schedule Cosine -gpu 0 -temperature 16 -dataroot ./data -from_scratch
 
 python train.py -project ssfe -dataset PlantVillage -base_mode 'ft_cos' -new_mode 'avg_cos' -gamma 0.1 -lr_base 0.1 \
 -lr_new 0.1 -decay 0.0005 -epochs_base 150 -schedule Milestone -milestones 60 90 -gpu 0 \
